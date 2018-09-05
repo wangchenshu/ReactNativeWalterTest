8a79f48d7d7cfa65af4bfb7cb83cc0a69c923468
wangchenshu <csw.dawm@gmail.com>
2018-09-05 18:53:55 +0800

    refactory

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,50 +5,48 @@ import { onIncrement, onDecrement } from '../reducers/reducers'
 import { View, Button, Text } from 'react-native'
 
 class Counter extends React.Component {
-    constructor(props) {
-        super(props)
-        this.incrementIfOdd = this.incrementIfOdd.bind(this)
-        this.incrementAsync = this.incrementAsync.bind(this)
-    }
+	constructor(props) {
+		super(props)
+		this.incrementIfOdd = this.incrementIfOdd.bind(this)
+		this.incrementAsync = this.incrementAsync.bind(this)
+	}
 
-    incrementIfOdd() {
-        if (this.props.value % 2 !== 0) {
-            this.props.onIncrement()
-        }
-    }
+	incrementIfOdd() {
+		if (this.props.value % 2 !== 0) {
+			this.props.onIncrement()
+		}
+	}
 
-    incrementAsync() {
-        setTimeout(this.props.onIncrement, 1000)
-    }
+	incrementAsync() {
+		setTimeout(this.props.onIncrement, 1000)
+	}
 
-    render() {
-        return (
-            <View>
-                <Text>Clicked: {this.props.value} times {' '}</Text>
-                <Button onPress={this.props.onIncrement} title="+" />
-                <Button onPress={this.props.onDecrement} title="-" />
-                <Button onPress={this.incrementIfOdd} title="increment if odd" />
-                <Button onPress={this.incrementAsync} title="increment async" />
-            </View>
-        )
-    }
+	render() {
+		return (
+			<View>
+				<Text>Clicked: {this.props.value} times</Text>
+				<Button onPress={this.props.onIncrement} title="+" />
+				<Button onPress={this.props.onDecrement} title="-" />
+				<Button onPress={this.incrementIfOdd} title="increment if odd" />
+				<Button onPress={this.incrementAsync} title="increment async" />
+			</View>
+		)
+	}
 }
 
 Counter.propTypes = {
-    value: PropTypes.number.isRequired,
-    onIncrement: PropTypes.func.isRequired,
-    onDecrement: PropTypes.func.isRequired
+	value: PropTypes.number.isRequired,
+	onIncrement: PropTypes.func.isRequired,
+	onDecrement: PropTypes.func.isRequired
 }
 
 const mapStatetoProps = (state) => {
-    return {
-        value: state
-    }
+	return {
+		value: state
+	}
 }
 
-const mapDispatchtoProps = {
-    onIncrement, onDecrement
-}
+const mapDispatchtoProps = {onIncrement, onDecrement}
 
 Counter = connect(mapStatetoProps, mapDispatchtoProps)(Counter)
 export default Counter
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,27 +3,27 @@
  * action creator
  */
 export function onIncrement() {
-    return {
-        type: 'INCREMENT'
-    }
+  return {
+    type: 'INCREMENT'
+  }
 }
 
 export function onDecrement() {
-    return {
-        type: 'DECREMENT'
-    }
+  return {
+    type: 'DECREMENT'
+  }
 }
 
 /*
  * reducer
  */
 export default counter = (state = 0, action) => {
-    switch(action.type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
-        default: 
-            return 10
-    }
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return 10
+  }
 }
