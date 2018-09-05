1eff3bb091911f0f01e25685a43776f5936fb272
wangchenshu <csw.dawm@gmail.com>
2018-09-05 18:03:09 +0800

    first commit

---
+++ src/reducers/reducers.js
@@ -0,0 +1,25 @@
+
+export function onIncrement() {
+    console.log('add')
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+export function onDecrement() {
+    console.log('del')
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+export default counter = (state = 0, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default: 
+            return 10
+    }
+}
